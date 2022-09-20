249276dcc6676f31d9fea243d014ccc2b0955d75
Muhammet Can Yıldız <94134588+Cann2000@users.noreply.github.com>
2022-09-20 17:42:15 +0300

    Create Cutter.cs

---
+++ Cutter.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EzySlice;  // !!!
+
+
+public class Cutter : MonoBehaviour
+{
+    Material mat;
+    GameObject CutObj;
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && CutObj != null)
+        {
+            SlicedHull kesilen = Cut(CutObj, mat);
+
+            //Kesilen Ust
+
+            GameObject CutTop = kesilen.CreateUpperHull(CutObj, mat);
+            CutTop.AddComponent<MeshCollider>().convex = true;
+            CutTop.AddComponent<Rigidbody>();
+            CutTop.layer = LayerMask.NameToLayer("Kesilebilir");
+
+            //Kesilen alt
+
+            GameObject CutBottom = kesilen.CreateLowerHull(CutObj, mat);
+            CutBottom.AddComponent<MeshCollider>().convex = true;
+            CutBottom.AddComponent<Rigidbody>();
+            CutBottom.layer = LayerMask.NameToLayer("Kesilebilir");
+
+            Destroy(CutObj);
+
+            CutObj = null;
+
+        }
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Kesilebilir"))
+        {
+            mat = other.gameObject.GetComponent<MeshRenderer>().material;
+            CutObj = other.gameObject;
+        }
+    }
+
+    public SlicedHull Cut(GameObject obj, Material crossSectionMaterial = null)
+    {
+        return obj.Slice(transform.position, transform.up, crossSectionMaterial);
+    }
+}
